88f67af7207cf8b1cb801c94f161a913163a5ea0
rafiqul-altersense <rafiqul.altersense@gmail.com>
2023-09-21 11:36:11 +0600

    init2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particleArray = [];
 
 window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
@@ -17,20 +18,72 @@ const mouse = {
 canvas.addEventListener("click", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  console.log(mouse);
-  drowCircle(mouse.x, mouse.y);
+  //   console.log(mouse);
+  //   drowCircle(mouse.x, mouse.y);
 });
 
 canvas.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  console.log(mouse);
-  drowCircle(mouse.x, mouse.y);
+  //   console.log(mouse);
+  //   drowCircle(mouse.x, mouse.y);
 });
 
-const drowCircle = function (x, y) {
+const drowCircle = function () {
   ctx.fillStyle = "blue";
   ctx.beginPath();
-  ctx.arc(x, y, 50, 0, Math.PI * 2);
+  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
   ctx.fill();
 };
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    // this.y = mouse.y;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particleArray.push(
+      new Particle(
+        mouse.x,
+        mouse.y,
+        Math.random() * 5 + 1,
+        Math.random() * 3 - 1.5,
+        Math.random() * 3 - 1.5
+      )
+    );
+  }
+}
+
+init();
+
+function handleParticle() {
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+    particleArray[i].draw();
+  }
+}
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticle()
+  requestAnimationFrame(animate);
+};
+
+animate();
